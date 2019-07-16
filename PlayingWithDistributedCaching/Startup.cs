d394cfa0cb9e44fd406c917e8579fa0933c0738d
Balázs Keresztúri <19balazs86@gmail.com>
2019-07-16 21:02:15 +0100

    Add project files.

---
+++ PlayingWithDistributedCaching/Startup.cs
@@ -0,0 +1,33 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace PlayingWithDistributedCaching
+{
+  public class Startup
+  {
+    private readonly IConfiguration _configuration;
+
+    public Startup(IConfiguration configuration)
+    {
+      _configuration = configuration;
+    }
+
+    public void ConfigureServices(IServiceCollection services)
+    {
+      services.AddControllers();
+    }
+
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+      if (env.IsDevelopment())
+        app.UseDeveloperExceptionPage();
+
+      app.UseRouting();
+
+      app.UseEndpoints(endpoints => endpoints.MapControllers());
+    }
+  }
+}
